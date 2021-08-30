53ac0e70fedf909a83474c55248dd73c65f9b8a6
JesusdelaGarza <a01411850@itesm.mx>
2021-08-30 00:00:15 -0500

    Realicé el ejercicio 1

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    x1 = float(input('Dame el x1: '))
-    y1 = float(input('Dame el y1: '))
-    x2 = float(input('Dame el x2: '))
-    y2 = float(input('Dame el y2: '))
+    x1 = float(input('Dame x1: '))
+    y1 = float(input('Dame y1: '))
+    x2 = float(input('Dame x2: '))
+    y2 = float(input('Dame y2: '))
     n = (y2 - y1) / (x2 - x1)
     print('Pendiente:', n)
 
